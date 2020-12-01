1272289fdc6c7ace1e2d3f52bc3f28a30fd83c37
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-12-01 11:22:47 -0500

    feat: add error handling

--- src/api/motivation.js
+++ src/api/motivation.js
@@ -2,11 +2,21 @@ const router = require("express").Router();
 const motivation = require("../controllers/motivation");
 
 router.get("/", (req, res, next) => {
-  res.json(motivation.getAll());
+  try {
+    const data = motivation.getAll();
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get("/random", (req, res, next) => {
-  res.json(motivation.getRandom());
+  try {
+    const data = motivation.getRandom();
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
