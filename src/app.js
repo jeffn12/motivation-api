72e28f51f19b8bd281ee2ecc4a6432531d3273a6
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-12-03 12:08:23 -0500

    feat: add ejs

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const ejs = require("ejs");
 
 require("dotenv").config();
 
@@ -17,11 +18,14 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use("/public", express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.json({
-    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄"
-  });
+  res.render("index");
+});
+app.get("/add", (req, res) => {
+  res.render("add-motivation");
 });
 
 app.use("/api/v1", api);
